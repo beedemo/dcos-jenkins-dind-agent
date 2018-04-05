23b9db4ed986c590c3b3f145a7ba9d16686e3fd0
Kurt Madel <kmadel@mac.com>
2018-04-05 19:45:45 -0400

    fix for dind exec

--- wrapper.sh
+++ wrapper.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-echo "==> Launching the Docker daemon..."
-dind dockerd --storage-driver=vfs $DOCKER_EXTRA_OPTS &
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
+	# add our default arguments
+	set -- dockerd \
+		--host=unix:///var/run/docker.sock \
+		--host=tcp://0.0.0.0:2375 \
+		"$@"
+fi
 
-while(! docker info > /dev/null 2>&1); do
-    echo "==> Waiting for the Docker daemon to come online..."
-    sleep 1
-done
-echo "==> Docker Daemon is up and running!"
+if [ "$1" = 'dockerd' ]; then
+	# if we're running Docker, let's pipe through dind
+	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
+	set -- sh "$(which dind)" "$@"
+fi
 
 /bin/sh -c "$@"
